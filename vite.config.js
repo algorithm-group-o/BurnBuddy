00d53c323ad9a8958058bbde8679735d58952c6c
김동현 <oksean030330@gmail.com>
2024-11-26 00:29:34 +0900

    image route fix

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,10 @@ export default defineConfig({
   plugins: [svelte()],
   base: '/BurnBuddy/',
   build: {
-    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        assetFileNames: 'assets/[name][extname]',
       }
     }
   }
