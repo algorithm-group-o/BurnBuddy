7d38dcc66f64833fdb8de0e10d61471c720484e8
김동현 <oksean030330@gmail.com>
2024-11-26 00:11:35 +0900

    dist assets route fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,15 @@ export default defineConfig({
   base: '/BurnBunny/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    sourcemap: true,
+    reportCompressedSize: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
   }
 })
